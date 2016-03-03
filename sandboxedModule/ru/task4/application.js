f9735a8b12f795048b92e04a4852018e7d77b43b
Yaroslav Oshyyko <yaroslav.oshiyko.5@gmail.com>
2016-03-03 10:20:33 +0200

    Completed task4

---
+++ sandboxedModule/ru/task4/application.js
@@ -0,0 +1,13 @@
+// Файл содержит маленький кусочек основного модуля демонстрационного
+// прикладного приложения, загружаемого в песочницу демонстрационным
+// кусочком фреймворка. Читайте README.md в нем задания.
+
+// Вывод из глобального контекста модуля
+console.log('From application global context');
+
+module.exports = function() {
+  // Вывод из контекста экспортируемой функции
+  console.log('From application exported function');
+};
+
+console.log("From wrapped console.log()");
---
+++ sandboxedModule/ru/task4/framework.js
@@ -0,0 +1,51 @@
+// Файл, демонстрирующий то, как фреймворк создает среду (песочницу) для
+// исполнения приложения, загружает приложение, передает ему песочницу в
+// качестве глобального контекста и получает ссылу на экспортируемый
+// приложением интерфейс. Читайте README.md в нем задания.
+
+// Фреймворк может явно зависеть от библиотек через dependency lookup
+var fs = require('fs'),
+    vm = require('vm'),
+    path  = require('path');
+
+// Создаем контекст-песочницу, которая станет глобальным контекстом приложения
+var context = {
+    module: {},
+    console: console,
+    console: {
+        log: function(message){
+            var date = new Date();
+            if(process.argv.length == 3){
+                applicationName = path.basename(process.argv[2]);
+            }
+            else{
+                applicationName = "application";
+            }
+            var time = date.getDate() + ':' + (date.getMonth()+1) + ':' + date.getFullYear() + '  ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+            console.log(applicationName + ' ' + time + ': ' + message);
+        }
+    }
+};
+context.global = context;
+var sandbox = vm.createContext(context);
+
+// Читаем исходный код приложения из файла
+var fileName;
+if(process.argv.length == 3){
+    fileName = process.argv[2] + ".js";
+}
+else{
+    fileName = "application.js";
+}
+
+
+fs.readFile(fileName, function(err, src) {
+  // Тут нужно обработать ошибки
+  //TODO
+  // Запускаем код приложения в песочнице
+  var script = vm.createScript(src, fileName);
+  script.runInNewContext(sandbox);
+  
+  // Забираем ссылку из sandbox.module.exports, можем ее исполнить,
+  // сохранить в кеш, вывести на экран исходный код приложения и т.д.
+});
