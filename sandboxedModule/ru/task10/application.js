6ee488b3009e22ab770839a1583a5e2fdc76a06a
Yaroslav Oshyyko <yaroslav.oshiyko.5@gmail.com>
2016-03-16 00:16:22 +0200

    Completed task10

---
+++ sandboxedModule/ru/task10/application.js
@@ -0,0 +1,22 @@
+// Файл содержит маленький кусочек основного модуля демонстрационного
+// прикладного приложения, загружаемого в песочницу демонстрационным
+// кусочком фреймворка. Читайте README.md в нем задания.
+
+require("util");
+// Вывод из глобального контекста модуля
+console.log('From application global context');
+
+module.exports = function() {
+  // Вывод из контекста экспортируемой функции
+  console.log('From application exported function');
+};
+
+console.log("From wrapped console.log()");
+
+console.dir(global);
+
+
+//add new key to global
+global.func = function(a,b){
+  return a + b;
+}
